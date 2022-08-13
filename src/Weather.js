1ab8bb8619f09e122cae1d9c58f74c58c983c08a
Lauren Main <lauren.main28@gmail.com>
2022-08-13 16:20:30 -0700

    adds single movieday component

--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { Accordion } from "react-bootstrap";
-import WeatherDay from "./WeatherDay"; 
+import WeatherDay from "./WeatherDay";
 
 class Weather extends React.Component {
-
-
   render() {
     return (
       <>
-          {this.props.weather.length > 0 &&
-            this.props.weather.map((day)=>(
-              <WeatherDay
-              idx = {day.idx}
-              day = {day.date}
+        {this.props.weather.length > 0 &&
+          this.props.weather.map((day) => (
+            <WeatherDay
+              idx={day.idx}
+              day={day.date}
               description={day.desciption}
-              />
-              ))}
-              </>
-    )
+            />
+          ))}
+      </>
+    );
   }
 }
 
-
-
-
 export default Weather;
-
